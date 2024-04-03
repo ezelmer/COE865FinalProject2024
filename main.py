95fab618cf4f36a09b1d07e6d191754bdb7ea657
Aaron Dougdeen <dougdeenaaron@gmail.com>
2024-04-03 17:02:33 -0400

    optimal path computation

--- main.py
+++ main.py
@@ -19,7 +19,6 @@ class path:
     def __eq__(self, other):
         return self.destIP == other.destIP and self.Cost == other.Cost and self.Capacity == other.Capacity and self.specPath == other.specPath
 
-
 class message:
     destIP = 0
     size = 0
@@ -27,23 +26,21 @@ class message:
         self.destIP = destIP
         self.size = size
 
-
-
 class Router:
     IP = 0
     paths = []
-    connections = []
+    links = []
     def __init__(self, IP):
         self.paths = []
-        self.connections = []
+        self.links = []
         self.IP = IP
         selfpath = path(self.IP, 0, 999, self.IP)
         self.paths.append(selfpath)
-    #advertised paths, connection used, and who we're receiving hello from.
-    def Hello(self, advpaths, theconnection, fromIP):
+    #advertised paths, link used, and who we're receiving hello from.
+    def Hello(self, advpaths, thelink, fromIP):
         for path in advpaths:
             if(self.IP not in path.specPath):
-                addpath = path.updateGivenPath(theconnection.cost, theconnection.capacity, self.IP)
+                addpath = path.updateGivenPath(thelink.cost, thelink.capacity, self.IP)
                 alreadyHaveIt = False
                 for path2 in self.paths:
                     if(path2 == addpath):
@@ -54,11 +51,10 @@ class Router:
 
 
     def SayHello(self):
-        for cxn in self.connections:
+        for cxn in self.links:
             cxn.router.Hello(self.paths, cxn, self.IP)
 
-
-class connection:
+class link:
     router = 0
     cost = 0
     capacity = 0
@@ -67,47 +63,48 @@ class connection:
         self.cost = cost
         self.capacity = capacity
 
-r1 = Router("10.10.10.1")
-r2 = Router("10.10.10.2")
-r3 = Router("10.10.10.3")
-print(str(r2.paths))
-print("\n\n\n")
-r1.connections.append(connection(r2, 3, 3))
-r2.connections.append(connection(r1, 3, 3))
-r3.connections.append(connection(r1, 5, 2))
+r1 = Router("ASN100")
+r2 = Router("ASN200")
+r3 = Router("ASN300")
+r4 = Router("ASN400")
+r5 = Router("ASN400:DC1")
 
-r3.connections.append(connection(r2, 1, 10))
-r2.connections.append(connection(r3, 1, 10))
-r1.connections.append(connection(r3, 5, 2))
+print(str(r5.paths))
+print("--------------------")
+r1.links.append(link(r2, 1, 1))
+r1.links.append(link(r3, 1, 1))
+r1.links.append(link(r4, 10, 1))
 
-r1.SayHello()
-r2.SayHello()
-r3.SayHello()
-r1.SayHello()
-r2.SayHello()
-r3.SayHello()
-r1.SayHello()
-r2.SayHello()
-r3.SayHello()
-r1.SayHello()
-r2.SayHello()
-r3.SayHello()
-r1.SayHello()
-r2.SayHello()
-r3.SayHello()
+r2.links.append(link(r1, 1, 1))
+r2.links.append(link(r3, 10, 1))
+r2.links.append(link(r4, 1, 1))
 
-print(str(r2.paths))
+r3.links.append(link(r1, 1, 1))
+r3.links.append(link(r2, 10, 1))
+r3.links.append(link(r4, 1, 1))
 
-#myIP = "10.10.10.0"
-#myCapacity = 999
-#rzeropath = path(myIP, 0, myCapacity, myIP)
+r4.links.append(link(r1, 10, 1))
+r4.links.append(link(r2, 1, 1))
+r4.links.append(link(r3, 1, 1))
+r4.links.append(link(r5, 0, 1))
 
-#r1addedpath = rzeropath.updateGivenPath(3, 10, "10.10.10.1")
+r5.links.append(link(r4, 0, 1))
 
-#rzeropaths = []
-#rzeropaths.append(rzeropath)
+for i in range(5):
+    r1.SayHello()
+    r2.SayHello()
+    r3.SayHello()
+    r4.SayHello()
+    r5.SayHello()
 
-#r1paths = []
-#r1paths.append()
-#print(str(r1addedpath))
 
+#Find optimal path
+for i in range (1,5):
+    weight=999
+    preferred= None
+    for p in r5.paths:
+        if (p.destIP == f'ASN{i}00' and (p.Cost/p.Capacity)<weight):
+            preferred = p
+            weight =p.Cost/p.Capacity
+    
+    print(preferred)
